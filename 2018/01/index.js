8a2ad6ee4b66913e834431b806f0b21e8e57b48d
Jeff DeButte <jdebutte@league.com>
2018-12-02 14:41:04 -0500

    initial commit and day 1 solution

---
+++ 2018/01/index.js
@@ -0,0 +1,29 @@
+import { reduce } from 'lodash/fp';
+import { reduce as _reduce } from 'lodash';
+import { inputs } from './inputs';
+
+export const part1 = reduce((total, value) => total + value, 0);
+
+export const part2 = values => {
+  const frequencies = [];
+  let frequency = 0;
+  let dupe;
+
+  while(!dupe) {
+    frequency = _reduce(values, (total, value) => {
+      const newValue = total + value;
+      if (!dupe && frequencies.includes(newValue)) {
+        dupe = newValue;
+      } else {
+        frequencies.push(newValue);
+      }
+
+      return newValue;
+    }, frequency);
+  }
+
+  return dupe;
+}
+
+export const answer1_1 = part1(inputs);
+export const answer1_2 = part2(inputs);
